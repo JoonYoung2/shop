c51c7c600c7de1da31128504ef7f8fcb463ad51a
Sua <kimsuasch@gmail.com>
2023-06-29 14:55:28 +0900

    sua second try

--- shop/src/product/ProductImpl.java
+++ shop/src/product/ProductImpl.java
@@ -1,5 +1,37 @@
 package product;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ProductImpl implements ProductService {
+	
+	private ProductDto register(String name, int price) {
+		ProductDto dto = new ProductDto();
+		dto.setName(name);
+		dto.setPrice(price);
+		return dto;
+	}
+	
+	public ProductDto register() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("상품명: ");
+		String name = input.next();
+		System.out.println("상품가격: ");
+		int price = input.nextInt();
+		
+		ProductDto result = register(name, price);
+		if(result != null) {
+			System.out.println("등록되었습니다.");
+		}else
+			System.out.println("문제가 발생했습니다.");
+		return result;
+	}
+
+	public void view(ArrayList<ProductDto> proList) {
+		for(int i=0; i<proList.size(); i++) {
+			System.out.println(i + "번째 상품은 "+proList.get(i).getName() + ", 가격은 " + proList.get(i).getPrice() + "입니다.");
+
+		}
+	}
 
 }
